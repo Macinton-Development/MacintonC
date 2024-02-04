a6c41efbe10aa53ccc38ef8856bdaaf80f1398db
Ivan <141495744+Macinton@users.noreply.github.com>
2024-02-04 20:50:53 +0300

    FIX CORE.py
    ARGUMENTS: PATHTOPROGRAMM DIRWITHMODULES

--- Macinton C Macro/CORE.py
+++ Macinton C Macro/CORE.py
@@ -6,13 +6,14 @@ def string_arrray_sum(array:list[str]):
         out += element
     return out
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     with open('MACINTONCMACRO.mcs') as reader:
-        lines = reader.readlines()
+        lines = reader.readlines()[1:]
     with open(sys.argv[1]) as reader:
         lines += reader.readlines()
     with open('out.mcs', 'w') as create: pass
     with open('out.mcs', 'a') as append:
+        append.write(f'@ : init : {sys.argv[2]} :\n')
         for line in lines:
             append.write(line)
         append.write('\n#}')
