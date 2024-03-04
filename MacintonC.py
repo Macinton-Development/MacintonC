120dcc124bdc7c9e3c955eb63b0eba0374708f84
Ivan <141495744+Macinton@users.noreply.github.com>
2024-03-04 21:22:18 +0300

    Macinton C 1.3 source code!
    Please, sorry me for this long period without updates because this month was hard for me. But Spring can be harder, so I think the next update will be in one-two monts. Also, I don't have ideas for this languages D=. But I think you will find some bugs or suggest some ideas. Bye!

--- MacintonC.py
+++ MacintonC.py
@@ -9,8 +9,11 @@ c_commands = []
 macinton_c_commands = []
 modules_c = []
 modules_dir = ''
+watchers = []
+watching = {}
 
 def get_args(line:str):
+    global watchers, watching
     out = []
     to_append = []
     is_string = 0
@@ -30,7 +33,10 @@ def get_args(line:str):
                 to_append = []
             else:
                 to_append.append(element)
-    return out[1:]
+
+    if out[0][1:-1] in watchers:
+        return [out[1:], 1]
+    return [out[1:]]
 
 def replace_with_args(line:str, args:list):
     global macinton_c_commands, c_commands
@@ -52,87 +58,124 @@ def string_array_sum(array:list):
         out += element
     return out
 
-def module_load(path_to_module_script:str, modules_dir:str):
-    global c_commands, macinton_c_commands, modules_c
+def module_load(path_to_module_script:str, modules_dir:str, _is_header=False):
+    global c_commands, macinton_c_commands, modules_c, watchers, watching
     with open(modules_dir + path_to_module_script) as module_script_reader:
         lines = module_script_reader.readlines()
     for line in lines:
-        if line[0] == '#' and (module := line[1:].replace('\n', '')) not in modules_c:
-            modules_c.append(module)
+        if line[0] == '#' and ((module := line[1:].replace('\n', '')) not in modules_c or not _is_header):
+            if not _is_header:
+                modules_c.append(module)
             with open('out.c', 'a') as append_include:
-                append_include.write('#include <' + module + '>\n')
+                if not _is_header:
+                    append_include.write('#include <' + module + '>\n')
+        elif line[0] == ';':
+            watchers.append(line[1:].replace('\n', ''))
         else:
-            line = line.split(' = ')
-            macinton_c_commands.append(line[0].replace('\n', ''))
-            with open(modules_dir + line[1].replace('\n', '')) as command_c:
-                c_commands.append(string_array_sum(command_c.readlines()) + '\n')    
+            if not _is_header:
+                line = line.split(' = ')
+                macinton_c_commands.append(line[0].replace('\n', ''))
+                with open(modules_dir + line[1].replace('\n', '')) as command_c:
+                    c_commands.append(string_array_sum(command_c.readlines()) + '\n')    
 
-def compilation(file_path:str):
-    global c_commands, macinton_c_commands, modules_dir
+def define_macinton_h_header(path:str):
+    name = path[:-3] + 'h'
+    with open(name, 'w') as create:
+        pass
+    compilation(path, name, False)
+    return name
+    
+
+def compilation(file_path:str, out_file:str, creation:bool):
+    global c_commands, macinton_c_commands, modules_dir, watching
     is_commented = 0
     is_block = 0
     current_block = ''
     c_code = 0
-    with open('out.c', 'w') as out_creation:
-        pass
+    if creation:
+        with open(out_file, 'w') as out_creation:
+            pass
     with open(file_path) as script_reader:
-        for line in script_reader.readlines():
-            if not is_commented and line.replace('\n', '').replace(' ', '') != '':
-                if line[0] == '@':
-                    args = get_args(line.replace('\n', ''))
-                    module_load(args[0], args[1])
-                elif line[0] == '*':
-                    c_code = not c_code
-                elif c_code:
-                    with open('out.c', 'a') as append_to_out:
-                        append_to_out.write(line)  
-                elif line[0] == '#' and not c_code:
-                    with open('out.c', 'a') as append_to_out:
+        with open(out_file, 'a') as append_to_out:
+            for line in script_reader.readlines():
+                if not is_commented and line.replace('\n', '').replace(' ', '') != '':
+                    if line[0] == '@':
+                        args = get_args(line.replace('\n', ''))[0]
+                        module_load(args[0], args[1])
+                    elif line[0] == '*':
+                        c_code = not c_code
+                    elif line[0] == '(':
+                        about = get_args(line[1:])[0]
+                        if about[0] not in watching:
+                            watching[about[0]] = [about[1], about[2]]
+                    elif line[0] == ')':
+                        if line[1:].replace('\n', '') in watching:
+                            watching.pop(line[1:].replace('\n', ''))
+                    elif c_code:
+                        append_to_out.write(line)
+                    elif line[0] == '#' and not c_code:
                         append_to_out.write(line[1:])
-                elif line[:2] == '->' and not is_block:
-                    is_block = 1
-                    with open('out.c', 'a') as append_to_out:
+                    elif line[:2] == '->' and not is_block:
+                        is_block = 1
                         append_to_out.write('void ' + line[2:] + '(void){\n')
-                elif line[:2] == '<-' and is_block:
-                    with open('out.c', 'a') as append_to_out:
+                    elif line[:2] == '<-' and is_block:
                         is_block = 0
                         append_to_out.write('}\n')
-                elif line[0] == '?':
-                    with open('out.c', 'a') as append_to_out:
+                    elif line[0] == '?':
                         args = get_args(line)
                         append_to_out.write('if(' + args[0] + '){\n' + args[1].replace('\n', '') + '();\n}\n')
-                elif line[0] == '$':
-                    with open('out.c', 'a') as append_to_out:
-                        append_to_out.write(replace_with_args(line[1:], get_args(line)))
-                elif line[0] == '!':
-                    with open('out.c', 'a') as append_to_out:
-                        append_to_out.write(f'#define {get_args(line)[0]} {get_args(line)[1]}\n')
-                elif line[0] == '~':
-                    with open('out.c', 'a') as append_to_out:
+                    elif line[0] == '$':
+                        args = get_args(line)
+                        append_to_out.write(replace_with_args(line[1:], args[0]))
+                        if len(args) - 1 and args[0][0] in watching:
+                            append_to_out.write('if(' + args[0][0] + ' == ' + watching[args[0][0]][0] + '){\n' + watching[args[0][0]][1] + '();\n}\n')
+                    elif line[0] == '!':
+                        append_to_out.write(f'#define {get_args(line)[0][0]} {get_args(line)[0][1]}\n')
+                    elif line[0] == '~':
                         append_to_out.write(f'#undef {line[1:]}\n')
-                elif line[:2] == '//':
-                    pass
-                elif line[0] == '[':
-                    is_commented = 1
-                elif line[:2] != '//':
-                    pass
+                    elif line[0] == '%':
+                        append_to_out.write(f'#include "' + define_macinton_h_header(line[1:].replace('\n', '')) + '"\n')
+                    elif line[0] == '&':
+                        append_to_out.write(f'#ifdef {line[1:].replace('\n', '')}\n')
+                    elif line[0] == '^':
+                        append_to_out.write(f'#else\n')
+                    elif line[0] == ';':
+                        append_to_out.write(f'#endif\n')
+                    elif line[0] == '/':
+                        append_to_out.write(f'ifndef {line[1:].replace('\n', '')}\n')
+                    elif line[:2] == '//':
+                        pass
+                    elif line[0] == '[':
+                        is_commented = 1
+                    elif line[:2] != '//':
+                        pass
+                    else:
+                        pass
+                elif is_commented and not is_block:
+                    if line[0] == ']':
+                        is_commented = 0
                 else:
-                    pass
-            elif is_commented and not is_block:
-                if line[0] == ']':
-                    is_commented = 0
-            else:
-                with open('out.c', 'a') as append_to_out:
                     if line[:2] == '<-':
                         is_block = 0
                         append_to_out.write('}\n')
-    
+
 if len(sys.argv) > 1:
-    compilation(sys.argv[1]) if sys.argv[1].lower() != '--help' else print('MacintonC 1.2 / Macinton 11\nFixed: Including one C library more than one time\nAdded: "->" and "<-" pointers\nAdded: --help and rm options\nAdded: ! pointer(equals to #define in C)\nAdded: ~ pointer(equals to #undef in C)\nJan2024')
     if sys.argv[1].lower() != '--help':
+        compilation(sys.argv[1], 'out.c', True)
         if os.path.getsize('out.c') > 0:
+            name = string_array_sum(sys.argv[1].split('.')[:-1])
+            if len(sys.argv) >= 3:
+                if sys.argv[2] == '?':
+                    name = sys.argv[3]
             if sys.platform == 'win32':
-                os.system('gcc -o ' + string_array_sum(sys.argv[1].split('.')[:-1]) + '.exe ' + 'out.c')
+                os.system('gcc -o ' + name + '.exe ' + 'out.c')
             else:
-                os.system('gcc -o' + string_array_sum(sys.argv[1].split('.')[:-1]) + ' out.c')
-            os.remove('out.c') if len(sys.argv) == 3 and sys.argv[2].lower() == 'rm' else print('Check out.c to get the C code')
+                os.system('gcc -o' + name + ' out.c')
+            if len(sys.argv) >= 3 and sys.argv[-1].lower() == 'rm':
+                os.remove('out.c')
+                print("No out.c's 0^o?")
+            else:
+                print('# #  ###\n# #  # #\n #   # #\n #   ### check the c code in out.c')
+    else:
+        print('MacintonC 1.3 / Macinton 12\nAdded:\n1.Macroses:\n\t& - #ifdef\n\t^ - #else\n\t; - #endif\n\t/ - #ifndef\n2.Watchers:\n\tAdd - ( : name : value : todo :\n\t\t*todo - equals to ?\n\tDecline - )Name\n3.Headers:\n\tInclude %path/to/header/name.mch\n4.New arguments:\n\t? - out name(without .exe/.app!!!)\n03/04/2024 - Moscow\nMM/DD/YYYY\nThanks for using, check new versions on our github!')
+    
